fb7944c9cc8b0e4391b7def8136cb2b6fb45f3ca
rpmk97 <rpmk97@gmail.com>
2018-07-27 19:14:29 +0530

    binary tree elementary

---
+++ binary_tree.c
@@ -0,0 +1,100 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct binary_tree{
+int d;
+
+struct binary_tree *left;
+struct binary_tree *right;
+
+};
+
+struct binary_tree *Insert(int,struct binary_tree*);
+void inorder(struct binary_tree*);
+
+
+void main(){
+
+struct binary_tree *root=NULL;
+
+
+int flag=1,ch,d;
+while(flag){
+printf("\nEnter Choice::\n1.Insert\n2.inorder traversal\n3.exit");
+scanf("%d",&ch);
+
+switch(ch){
+case 1:printf("\nEnter data::");
+scanf("%d",&d);
+root=Insert(d,root);
+break;
+
+case 2:inorder(root);
+break;
+
+case 3:flag=0;
+break;
+
+default:printf("\nTry Again\n");
+
+}
+}
+}
+
+
+struct binary_tree *Insert(int d,struct binary_tree *r){
+if(r==NULL)
+{
+r=(struct binary_tree*)malloc(sizeof(struct binary_tree));
+r->d=d;
+r->right=NULL;
+r->left=NULL;
+}
+
+else if(d<r->d)
+r->left=Insert(d,r->left);
+
+else if(d>r->d)
+r->right=Insert(d,r->right);
+else
+printf("\nduplicate key\n");
+
+return r;
+}
+
+
+
+void inorder(struct binary_tree *r){
+if(r==NULL)
+return;
+
+inorder(r->left);
+printf("%d",r->d);
+inorder(r->right);
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
